0471e1b1553c9f2fece7297a2e0386e6464621c6
Alexander Kutsenko <AlexStanley@users.noreply.github.com>
2023-05-22 22:10:15 +0300

    AddUser method code refactoring for UserService

--- LegacyApp/UserService.cs
+++ LegacyApp/UserService.cs
@@ -4,78 +4,76 @@ namespace LegacyApp
 {
     public class UserService
     {
-        public bool AddUser(string firname, string surname, string email, DateTime dateOfBirth, int clientId)
+        private readonly ClientRepository clientRepository;
+        private readonly UserCreditServiceClient userCreditService;
+
+        public UserService()
         {
-            if (string.IsNullOrEmpty(firname) || string.IsNullOrEmpty(surname))
-            {
+            clientRepository = new ClientRepository();
+            userCreditService = new UserCreditServiceClient();
+        }
+
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfUserBirth, int clientId)
+        {
+            if (IsNotValidFirstNameOrLastName(firstName, lastName))
                 return false;
-            }
 
-            if (email.Contains("@") && !email.Contains("."))
-            {
+            if (!IsValidEmail(email))
                 return false;
-            }
 
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
-            {
-                age--;
-            }
-
-            if (age < 21)
-            {
+            if (CalculateUserAge(dateOfUserBirth) < 21)
                 return false;
-            }
 
-            var clientRepository = new ClientRepository();
             var client = clientRepository.GetById(clientId);
 
             var user = new User
             {
                 Client = client,
-                DateOfBirth = dateOfBirth,
+                DateOfBirth = dateOfUserBirth,
                 EmailAddress = email,
-                Firstname = firname,
-                Surname = surname
+                Firstname = firstName,
+                Surname = lastName
             };
 
             if (client.Name == "VeryImportantClient")
-            {
-                // Skip credit chek
                 user.HasCreditLimit = false;
-            }
-            else if (client.Name == "ImportantClient")
-            {
-                // Do credit check and double credit limit
-                user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
-                    creditLimit = creditLimit * 2;
-                    user.CreditLimit = creditLimit;
-                }
-            }
             else
             {
-                // Do credit check
                 user.HasCreditLimit = true;
-                using (var userCreditService = new UserCreditServiceClient())
-                {
-                    var creditLimit = userCreditService.GetCreditLimit(user.Firstname, user.Surname, user.DateOfBirth);
-                    user.CreditLimit = creditLimit;
-                }
+                var creditLimit = userCreditService.GetCreditLimit(firstName, lastName, dateOfUserBirth);
+
+                if (client.Name == "ImportantClient")
+                    creditLimit *= 2;
+
+                user.CreditLimit = creditLimit;
             }
 
             if (user.HasCreditLimit && user.CreditLimit < 500)
-            {
                 return false;
-            }
-            
+
             UserDataAccess.AddUser(user);
 
             return true;
+        }
+
+        private bool IsNotValidFirstNameOrLastName(string firstName, string lastName)
+            => string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName);
+
+        private bool IsValidEmail(string email)
+            => email.Contains("@") && email.Contains(".");
+
+        private int CalculateUserAge(DateTime dateOfUserBirth)
+        {
+            var todayDate = DateTime.Now;
+            int userFullAge = todayDate.Year - dateOfUserBirth.Year;
+
+            if (todayDate.Month < dateOfUserBirth.Month
+                || (todayDate.Month == dateOfUserBirth.Month && todayDate.Day < dateOfUserBirth.Day))
+            {
+                userFullAge -= 1;
+            }
+
+            return userFullAge;
         }
     }
 }
