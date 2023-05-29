b83b4d111ae9db2afb1f6175b9544e5f47335356
Alexander Kutsenko <AlexStanley@users.noreply.github.com>
2023-05-29 13:44:18 +0300

    Adding refactoring for UserCreditService

---
+++ GrpcService/Services/UserCreditService.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using GrpcService.Protos;
+
+namespace GrpcService.Services
+{
+    public class UserCreditService : UserCredit.UserCreditBase
+    {
+        private readonly ILogger<UserCreditService> _logger;
+        public UserCreditService(ILogger<UserCreditService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<UserCreditResponse> GetCreditLimit(UserCreditRequest request, ServerCallContext context)
+        {
+            return base.GetCreditLimit(request, context);
+        }
+    }
+}
--- LegacyApp/UserCreditService.cs
+++ LegacyApp/UserCreditService.cs
@@ -1,46 +1,27 @@
-﻿using System;
-using System.CodeDom.Compiler;
-using System.Diagnostics;
-using System.ServiceModel;
-using System.ServiceModel.Channels;
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using GrpcService.Protos;
+using System;
 
 namespace LegacyApp
 {
-    [GeneratedCode("System.ServiceModel", "4.0.0.0")]
-    [ServiceContract(ConfigurationName =  "LegacyApp.IUserCreditService")]
-    public interface IUserCreditService
+    public partial class UserCreditServiceClient
     {
-        [OperationContract(Action = "http://totally-real-service.com/IUserCreditService/GetCreditLimit")]
-        int GetCreditLimit(string firstname, string surname, DateTime dateOfBirth);
-    }
-
-    [GeneratedCode("System.ServiceModel", "4.0.0.0")]
-    public interface IUserCreditServiceChannel : IUserCreditService, IClientChannel
-    {
-    }
-    
-    [DebuggerStepThrough]
-    [GeneratedCode("System.ServiceModel", "4.0.0.0")]
-    public partial class UserCreditServiceClient : ClientBase<IUserCreditService>, IUserCreditService
-    {
-        private IUserCreditServiceChannel _userCreditServiceChannelImplementation;
-        public UserCreditServiceClient() {}
-        
-        public UserCreditServiceClient(string endpointConfigurationName) : 
-            base(endpointConfigurationName)
-        {}
-        
-        public UserCreditServiceClient(string endpointConfigurationName, EndpointAddress remoteAddress) : 
-            base(endpointConfigurationName, remoteAddress)
-        {}
-        
-        public UserCreditServiceClient(Binding binding, EndpointAddress remoteAddress) : 
-            base(binding, remoteAddress)
-        {}
-
-        public int GetCreditLimit(string firstname, string surname, DateTime dateOfBirth)
+       public int GetCreditLimit(string firstNane, string lastName, DateTime dateOfBirth)
         {
-            return base.Channel.GetCreditLimit(firstname, surname, dateOfBirth);
+            var channel = GrpcChannel.ForAddress("http://totally-real-service.com/IUserCreditService/GetCreditLimit");
+            var userClient = new UserCredit.UserCreditClient(channel);
+
+            UserCreditRequest request = new UserCreditRequest
+            {
+                Firstname = firstNane,
+                Surname = lastName,
+                DateOfBirth = dateOfBirth.ToTimestamp()
+            };
+
+            var creditLimit = userClient.GetCreditLimit(request);
+
+            return creditLimit.CreditLimit;
         }
     }
 }
